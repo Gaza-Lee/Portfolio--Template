312d82db93ea4a998914e88439c4824bf7152176
GazaLee <67987968+Gaza-Lee@users.noreply.github.com>
2025-06-06 16:41:51 +0000

    Included Experince section

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ function toggleMenu(){
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.querySelector(".experience__details");
+    const leftArrow = document.querySelector(".arrows span:first-child");
+    const rightArrow = document.querySelector(".arrows span:last-child");
+
+    if (container && leftArrow && rightArrow) {
+
+        leftArrow.addEventListener("click", () => {
+            container.scrollBy({
+                left: -300,
+                behavior: "smooth"
+            });
+        });
+
+        rightArrow.addEventListener("click", () => {
+            container.scrollBy({
+                left: 300,
+                behavior: "smooth"
+            });
+        });
+    }
+});
