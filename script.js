821724ffb0d40f0305b7e40a03364b2a25ca1abe
GazaLee <67987968+Gaza-Lee@users.noreply.github.com>
2025-06-27 09:59:24 +0000

    -Introduce fade in animation on few sections and divs -Minor changes to UI to improve responsiveness and design

--- script.js
+++ script.js
@@ -89,3 +89,18 @@ document.addEventListener("DOMContentLoaded", function () {
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: "smooth" });
 }
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const faders = document.querySelectorAll('.fade-in-section');
+    const appearOptions = { threshold: 0.15 };
+    const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) return;
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+        });
+    }, appearOptions);
+
+    faders.forEach(fader => appearOnScroll.observe(fader));
+});
